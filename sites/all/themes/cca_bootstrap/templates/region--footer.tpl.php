661e19088d51a129b3a30da2833968d249f29386
Aaron Spafford <aaron.spafford@gmail.com>
2014-07-31 16:01:52 -0700

    override footer.tpl.php

---
+++ sites/all/themes/cca_bootstrap/templates/region--footer.tpl.php
@@ -0,0 +1,25 @@
+  <div class="navbar navbar-static-bottom">
+
+    <div class="row">
+    <div class="col-lg-12">
+    <ul class="list-unstyled">
+      <li class="pull-right">
+        help desk
+      </li>
+      <li>
+        cca.edu
+      </li>
+      <li>
+        your IP address is <?php echo $_SERVER["REMOTE_ADDR"]; ?>
+      </li>
+      <li>
+      printers.cca.edu
+      </li>
+      <li>
+      lorem ipsum link
+      </li>
+    </ul>
+    </div>
+    </div>
+
+</div>
