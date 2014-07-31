d3242984e1dc8ec41b758cc2b99f6ebff507c3bc
Aaron Spafford <aaron.spafford@gmail.com>
2014-07-31 16:01:24 -0700

    add font-awesome to template.php

--- sites/all/themes/cca_bootstrap/template.php
+++ sites/all/themes/cca_bootstrap/template.php
@@ -4,3 +4,12 @@
  * @file
  * template.php
  */
+
+function cca_bootstrap_preprocess_html(&$vars) {
+
+  // Load font-awesome
+  drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css', array(
+    'type' => 'external'
+  ));
+
+}
