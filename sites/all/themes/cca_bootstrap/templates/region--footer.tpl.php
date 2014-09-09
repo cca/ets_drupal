24776439844e8c5cd1505543e4f8cfc00b803b03
Aaron Spafford <aaron.spafford@gmail.com>
2014-09-09 14:02:01 -0700

    add footer tabs

--- sites/all/themes/cca_bootstrap/templates/region--footer.tpl.php
+++ sites/all/themes/cca_bootstrap/templates/region--footer.tpl.php
@@ -2,6 +2,21 @@
   <footer<?php print $attributes; ?>>
     <?php if ($content_attributes): ?><div<?php print $content_attributes; ?>><?php endif; ?>
     <?php //print $content; ?>
+    <div class="row footer-tabs">
+      <div class="col-xs-12">
+      <ul>
+        <li><a>Moodle</a></li>
+        <li><a>WebAdvisor</a></li>
+        <li><a>Google Apps</a></li>
+        <li><a>Vault</a></li>
+        <li><a>Libraries</a></li>
+        <li><a>Workday</a></li>
+        <li><a>Lynda</a></li>
+        <li><a>Papercut</a></li>
+      </ul>
+      </div>
+    </div>
+    <hr>
     <ul class="list-unstyled">
       <li class="pull-right">
         your IP address is <?php echo $_SERVER["REMOTE_ADDR"]; ?>
