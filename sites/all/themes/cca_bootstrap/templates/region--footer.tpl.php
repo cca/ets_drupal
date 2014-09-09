cb2344d62f5df8da7fd4fe6235f7e1da2e376f61
Aaron Spafford <aaron.spafford@gmail.com>
2014-09-09 13:07:58 -0700

    remove links from footer

--- sites/all/themes/cca_bootstrap/templates/region--footer.tpl.php
+++ sites/all/themes/cca_bootstrap/templates/region--footer.tpl.php
@@ -4,19 +4,12 @@
     <?php //print $content; ?>
     <ul class="list-unstyled">
       <li class="pull-right">
-        help desk
+        your IP address is <?php echo $_SERVER["REMOTE_ADDR"]; ?>
       </li>
       <li>
         cca.edu
       </li>
       <li>
-        your IP address is <?php echo $_SERVER["REMOTE_ADDR"]; ?>
-      </li>
-      <li>
-      printers.cca.edu
-      </li>
-      <li>
-      lorem ipsum link
       </li>
     </ul>
     <?php if ($content_attributes): ?></div><?php endif; ?>
