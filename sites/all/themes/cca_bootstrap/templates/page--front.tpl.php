66a43768f95000d08b65095328df84adeb74c9db
Aaron Spafford <aaron.spafford@gmail.com>
2014-09-12 12:43:38 -0700

    add 'Educational Technology Services' to homepage, rename classes

--- sites/all/themes/cca_bootstrap/templates/page--front.tpl.php
+++ sites/all/themes/cca_bootstrap/templates/page--front.tpl.php
@@ -4,17 +4,20 @@
 
 <div class="main-container container">
 
-  <div class="row row-wireframe row-1">
+  <h3>Educational Technology Services</h3>
+
+  <div class="row quicklinks">
     <div class="col-xs-12 col-sm-6 col-md-4"><h4>CCA Acounts</h4></div>
     <div class="col-xs-12 col-sm-6 col-md-4"><h4>Help Desk</h4></div>
     <div class="col-xs-12 col-sm-6 col-md-4"><h4>Hours</h4></div>
     <div class="col-xs-12 col-sm-6 col-md-4"><h4>Wireless</h4></div>
     <div class="col-xs-12 col-sm-6 col-md-4"><h4>Printers</h4></div>
     <div class="col-xs-12 col-sm-6 col-md-4"><h4>Equipment Checkout</h4></div>
-  </div>
+  </div><!-- /.quicklinks -->
+
   <div class="row row-wireframe row-2">
 
-    <div class="col-sm-8 col-wireframe">
+    <div class="col-sm-8 announcements">
       <h3>ANNOUNCEMENTS</h3>
       <div class="padding-base-horizontal">
         <h2>Lorem Ipsum Headline</h2>
@@ -26,8 +29,8 @@
         <h2>Lorem Ipsum Headline</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget ullamcorper dui, a varius nisl.</p>
       </div>
-    </div>
-    <div class="col-sm-4 col-wireframe resources">
+    </div><!-- /.announcements -->
+    <div class="col-sm-4 resources">
       <h3>RESOURCES</h3>
       <div><h4>Students</h4></div>
       <div><h4>Faculty & Staff</h4></div>
@@ -35,5 +38,5 @@
     </div>
 
   </div>
-</div>
+</div><!-- /#main-container -->
 <?php print render($page['footer']); ?>
