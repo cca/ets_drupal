8bd54c1e0d4e72e0e1687e9986aa9c9ffbe09b30
Aaron Spafford <aaron.spafford@gmail.com>
2014-10-07 16:08:04 -0700

    add prompt for username

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,13 @@
 from fabric.api import settings, abort, env, run, sudo
+from fabric.operations import prompt
 from fabric.context_managers import cd
 from fabric.contrib.files import exists
  
 def dev():
 	env.hosts = ['209.40.90.63']
-	env.user = 'aspafford'
 	env['ets_dir'] = '/opt/drupal/ets14/'
+	# prompt for username
+	env.user = prompt('Log in as user:', default=env.user)
  
 def push():
 	with cd(env['ets_dir']):
