8e52533cee242273676b3f60d8336cce1a0eb712
Aaron Spafford <aaron.spafford@gmail.com>
2014-11-10 16:39:32 -0800

    add jquery for interactive helpdesk form

--- sites/all/themes/cca_bootstrap/js/main.js
+++ sites/all/themes/cca_bootstrap/js/main.js
@@ -3,5 +3,37 @@
   // This is the shortened form of $(document).ready().
   $(function() {
 
+    // help desk form campus building and lab options
+  	$oakBuildingOptions = $('.webform-component--oakland-building-options').hide();
+    $oakLabOptions = $('.webform-component--oakland-lab-options').hide();
+  	$sfBuildingOptions = $('.webform-component--sf-building-options').hide();
+    $sfLabOptions = $('.webform-component--sf-lab-options').hide();
+  	$labCheck = $('.webform-component--lab-check').hide();
+
+  	$('.webform-component--campus, .webform-component--lab-check').change(function() {
+
+			var campusValue = $( "input:radio:checked" ).val();
+      var labValue = $(".webform-component--lab-check input:radio:checked").val();
+
+			$labCheck.show();
+
+			if (campusValue == 'Oakland') {
+				($sfBuildingOptions, $sfLabOptions).hide();
+				$oakBuildingOptions.show();
+        $labOptions = $oakLabOptions;
+			} else {
+				($oakBuildingOptions, $oakLabOptions).hide();
+				$sfBuildingOptions.show();
+        $labOptions = $sfLabOptions;
+			}
+
+      if (labValue == 'yes') {
+        $labOptions.show();
+      } else {
+        $labOptions.hide();
+      }
+
+  	});
+
   });
 })(jQuery);
